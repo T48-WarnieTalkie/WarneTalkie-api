8e513af5f5df7f7f22b37ffd8ae434e791b796d2
tomcifa <t.cifariellociardi@studenti.unitn.it>
2025-02-11 20:29:35 +0100

    updated populate script to check if users already exist

--- populatedbtest.js
+++ populatedbtest.js
@@ -11,33 +11,47 @@ async function main() {
     console.log('About to connect');
     await mongoose.connect(mongoDB);
     console.log('Should be connected?');
-    const adminSensitive = new UserSensitive({
+    const existingModSensitive = await UserSensitive.findOne({
         emailAddress: "mod@mod.com",
         cellphoneNumber: "0000000000",
-        password: "$2a$12$.vI9hho4ewT7q4pkYcNbXunaVAHj1dLwFomTewxzhbGPvZ.hYTC8u" //Moderator1
-    })
-    await adminSensitive.save();
-    const admin = new User({
-        name: "Moderator",
-        surname: "Moderator",
-        isModerator: true,
-        userSensitiveID: adminSensitive._id
-    })
-    await admin.save();
-    console.log("saved moderator");
-    const userSensitive = new UserSensitive({
+        password: "$2a$12$.vI9hho4ewT7q4pkYcNbXunaVAHj1dLwFomTewxzhbGPvZ.hYTC8u"
+    }).exec()
+    if(!existingModSensitive) {
+        const modSensitive = new UserSensitive({
+            emailAddress: "mod@mod.com",
+            cellphoneNumber: "0000000000",
+            password: "$2a$12$.vI9hho4ewT7q4pkYcNbXunaVAHj1dLwFomTewxzhbGPvZ.hYTC8u" //Moderator1
+        })
+        await modSensitive.save();
+        const mod = new User({
+            name: "Moderator",
+            surname: "Moderator",
+            isModerator: true,
+            userSensitiveID: modSensitive._id
+        })
+        await mod.save();
+        console.log("saved moderator");
+    }
+    const existingUserSensitive = await UserSensitive.findOne({
         emailAddress: "standardUser@standardUser.com",
         cellphoneNumber: "1111111111",
-        password: "$2a$12$VvjnpXeL.gsj/Nv2FsiK4.NsnbYU3w4CHTNBbCiqUsVhKV901G/Wm" //StandardUser1
-    })
-    await userSensitive.save()
-    const user = new User({
-        name: "StandardUser",
-        surname: "StandardUser",
-        isModerator: false,
-        userSensitiveID: userSensitive._id
-    })
-    await user.save()
-    console.log("saved standard user")
+        password: "$2a$12$VvjnpXeL.gsj/Nv2FsiK4.NsnbYU3w4CHTNBbCiqUsVhKV901G/Wm"
+    }).exec()
+    if(!existingUserSensitive) {
+        const userSensitive = new UserSensitive({
+            emailAddress: "standardUser@standardUser.com",
+            cellphoneNumber: "1111111111",
+            password: "$2a$12$VvjnpXeL.gsj/Nv2FsiK4.NsnbYU3w4CHTNBbCiqUsVhKV901G/Wm" //StandardUser1
+        })
+        await userSensitive.save()
+        const user = new User({
+            name: "StandardUser",
+            surname: "StandardUser",
+            isModerator: false,
+            userSensitiveID: userSensitive._id
+        })
+        await user.save()
+        console.log("saved standard user")
+    }
     mongoose.connection.close();
 }

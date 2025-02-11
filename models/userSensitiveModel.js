fc8280366ed59dc575bc482a2ddde36c417b3ec6
tomcifa <t.cifariellociardi@studenti.unitn.it>
2025-02-11 22:53:15 +0100

    added email validation

--- models/userSensitiveModel.js
+++ models/userSensitiveModel.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSensitiveSchema = new Schema({
-    cellphoneNumber: {type: String, required: true, unique: true, dropDups: true},
-    emailAddress: {type: String, required: true, unique: true, dropDups: true},
+    cellphoneNumber: {type: String, required: true, unique: true, dropDups: true, minLenght: 10, maxLength: 10},
+    emailAddress: {type: String, required: true, unique: true, dropDups: true, validate: [isValidEmail, "Invalid email"]},
     password: {type: String, required: true, immutable: true, validate: [isValidPassword, "Invalid password"]}
 })
 
 function isValidPassword(val) {
     return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(val)
+}
+
+function isValidEmail(val) {
+    return /.*@.*/.test(val)
 }
 module.exports = mongoose.model('UserSensitive', userSensitiveSchema);

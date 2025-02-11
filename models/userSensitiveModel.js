6a9e44bae4319fe05453774c695f4a41277ccea4
tomcifa <t.cifariellociardi@studenti.unitn.it>
2025-02-12 00:49:44 +0100

    fixed typo

--- models/userSensitiveModel.js
+++ models/userSensitiveModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSensitiveSchema = new Schema({
-    cellphoneNumber: {type: String, required: true, unique: true, dropDups: true, minLenght: 10, maxLength: 10},
+    cellphoneNumber: {type: String, required: true, unique: true, dropDups: true, minLength: 10, maxLength: 10},
     emailAddress: {type: String, required: true, unique: true, dropDups: true, validate: [isValidEmail, "Invalid email"]},
     password: {type: String, required: true, immutable: true, validate: [isValidPassword, "Invalid password"]}
 })

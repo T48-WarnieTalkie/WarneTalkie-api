afb152561c28d4319d0c0f6c4665b242a5781403
tomcifa <t.cifariellociardi@studenti.unitn.it>
2025-02-11 16:25:31 +0100

    password validation

--- models/userSensitiveModel.js
+++ models/userSensitiveModel.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema;
 const userSensitiveSchema = new Schema({
     cellphoneNumber: {type: String, required: true, unique: true, dropDups: true},
     emailAddress: {type: String, required: true, unique: true, dropDups: true},
-    password: {type: String, required: true, immutable: true}
+    password: {type: String, required: true, immutable: true, validate: [isValidPassword, "Invalid password"]}
 })
 
+function isValidPassword(val) {
+    return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(val)
+}
 module.exports = mongoose.model('UserSensitive', userSensitiveSchema);

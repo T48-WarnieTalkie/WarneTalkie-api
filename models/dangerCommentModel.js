04f83b77f30948f50b65149c25a76445ca79a5ef
tomcifa <t.cifariellociardi@studenti.unitn.it>
2025-02-11 14:52:51 +0100

    renamed validation function

--- models/dangerCommentModel.js
+++ models/dangerCommentModel.js
@@ -5,11 +5,11 @@ const Schema = mongoose.Schema;
 const dangerCommentSchema = new Schema({
     userID: {type: mongoose.Schema.Types.ObjectId, ref:'User', required: true},
     dangerID: {type: mongoose.Schema.Types.ObjectId, ref:'Danger', required: true},
-    sendTimestamp: {type: Date, required: true, validate: [dateValidator, 'Must be a past date, got {VALUE}']},
+    sendTimestamp: {type: Date, required: true, validate: [hasTwoElements, 'Must be a past date, got {VALUE}']},
     text: {type: String, required: true, maxLenght: 120}
 })
 
-function dateValidator(val) { 
+function hasTwoElements(val) { 
     return val <= new Date()
 }
 

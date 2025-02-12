18122497ca5a3f70d99cb3c2b8d01db7acc0d441
tomcifa <t.cifariellociardi@studenti.unitn.it>
2025-02-12 02:40:41 +0100

    added cellphonenum validator

--- models/userSensitiveModel.js
+++ models/userSensitiveModel.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSensitiveSchema = new Schema({
-    cellphoneNumber: {type: String, required: true, unique: true, dropDups: true, minLength: 10, maxLength: 10},
+    cellphoneNumber: {type: String, required: true, unique: true, dropDups: true, minLength: 10, maxLength: 10, validate: [isValidCellphoneNumber, "Invalid cellphone number"]},
     emailAddress: {type: String, required: true, unique: true, dropDups: true, validate: [isValidEmail, "Invalid email"]},
     password: {type: String, required: true, immutable: true, validate: [isValidPassword, "Invalid password"]}
 })
+
+function isValidCellphoneNumber(val) {
+    return /[0-9]{10}/.test(val)
+}
 
 function isValidPassword(val) {
     return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(val)
